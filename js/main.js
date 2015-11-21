320e365a3423cca460fb5a317ff5ca438f852308
harshai <harshai@thoughtworks.com>
2015-11-21 20:24:27 +0530

    Polishing the interactions, fixing up the creations section

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 $(function(){
   var $main = $('#main').append($.parseHTML($("noscript").text())),
-      $creations = $('#creations').addClass('hidden');
+      $creations = $('#creations').addClass('hidden'),
+      $scrollBtn = $('.scroll').remove();
+
+  $main.on('click', '.scroll' ,function(e){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 250);
+      $(this).blur();
+    return false;
+  });
+
   $('#intro').t({
-    speed: 25,
     mistype: 0.5,
+    speed: 25,
     speed_vary:true,
     prefix: "> ",
     tag: "code",
+    init: function($el) {
+      $el.prepend('<ins><span class="prefix"></span></ins>')
+      $el.wrapInner("<div class='inner' />")
+    },
     fin: function($el){
-      $el.append('<div class="bounce" />')
+      $el.append($scrollBtn)
       $creations.removeClass("hidden");
     }
   })
